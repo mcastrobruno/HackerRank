dbede6fe8d1d9e6b29d7f0744296f756273b1840
Bruno Castro <mcastrobruno@gmail.com>
2016-09-03 23:06:36 -0300

    Initial

---
+++ Challenges/CircularArrayRotation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Challenges
+{
+    class CircularArrayRotation
+    {
+        /*
+        n = número de elementos
+        k = número de rotações
+        q = número de queries
+        */
+
+        public void Run()
+        {
+            int n, k, q;
+
+            Console.WriteLine("Primeira Linha (Parâmetros): ");
+            string line1 = Console.ReadLine();
+
+            n = int.Parse(line1.Split(' ')[0]);
+            k = int.Parse(line1.Split(' ')[1]);
+            q = int.Parse(line1.Split(' ')[2]);
+
+
+            Console.WriteLine("Segunda Linha (Elementos): ");
+            string line2 = Console.ReadLine();
+            int[] elementos = Array.ConvertAll(line2.Split(' '), s => int.Parse(s));
+
+
+            while (k > 0)
+            {
+
+
+            }
+
+
+            for (int i = 0; i < q; i++)
+            {
+                Console.WriteLine("Consulta (Elementos): ");
+                int query = int.Parse(Console.ReadLine());
+                Console.WriteLine(elementos[query]);
+            }
+
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using HackerRank.Challenges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircularArrayRotation r = new CircularArrayRotation();
+            r.Run();
+        }
+
+
+
+    }
+}
